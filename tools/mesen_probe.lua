33965e52d5ab5fae3f364533f35d76a4429543f9
macaon <marcus@skogangen.se>
2026-04-24 23:33:12 +0200

    chore: drop one-off probe.lua scratch script

--- tools/mesen_probe.lua
+++
@@ -1,6 +0,0 @@
--- Does os module exist?
-emu.displayMessage("T", "os=" .. tostring(os))
-if os and os.getenv then
-  emu.displayMessage("T", "has getenv; T=" .. tostring(os.getenv("TRACE_CYCLES")))
-end
-emu.stop(0)
